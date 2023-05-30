2f5695e9ca84871093fa6063ac767a7e93d7f2a8
jwkaterina <jwkaterina@gmail.com>
2023-05-30 13:35:55 +0200

    create repository

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import "./css/main.css";
+import "./css/mobile.css";
+
+import { sum } from "./utils";
+
+console.log("Hello, Theodor!", sum(3, 2));
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const postCssNormalize = require("postcss-normalize");
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    client: {
+      overlay: true
+    },
+    hot: true,
+    // static: {
+    //   directory: path.join(__dirname, "./"),
+    // },
+    watchFiles: ["src/*"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: Object.assign({}, undefined),
+          },
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("postcss-flexbugs-fixes"),
+                  require("postcss-preset-env")({
+                    autoprefixer: {
+                      flexbox: "no-2009"
+                    },
+                    stage: 3
+                  }),
+                  postCssNormalize()
+                ]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|map)$/i,
+        type: "asset/resource"
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin(),
+    // new CopyWebpackPlugin({
+    //   patterns: [{from: "src/img", to: "img"}]
+    // })
+  ]
+};
