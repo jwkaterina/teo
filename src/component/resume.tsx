65e63affcc582213a68ebad2193c288874054d04
jwkaterina <jwkaterina@gmail.com>
2023-06-22 17:07:43 +0200

    add values for inputs and useEffect for animation

--- src/component/resume.tsx
+++ src/component/resume.tsx
@@ -16,13 +16,21 @@ export const Resume = () => {
     const {setOpenState} = Reactish.useContext(OpenPageContext);
     const [data, setData] = Reactish.useState(initData);
 
-    const [subsequentRun, setSubsequentRun] = Reactish.useState(false);
-    setSubsequentRun(true);
+    const evaluateClass = (className: string): string => {
+        Reactish.useEffect([OpenState], () => {
+            return className
+        })
+        return ""
+    }
+    
+
+    let dateValue: string;
+    let weightValue: number;
 
     const addData = () => {
         const dataUnit: DataUnit = [
-            (document.getElementById("date") as HTMLInputElement).value, 
-            parseInt((document.getElementById("weight") as HTMLInputElement).value)
+            dateValue, 
+            weightValue
         ]
         data.push(dataUnit)
         setData(data);
@@ -55,7 +63,7 @@ export const Resume = () => {
                 },
                 animation: {
                     startup: true,
-                    duration: 1000,
+                    duration: 700,
                     easing: 'inAdnOut',
                 }
             };
@@ -69,24 +77,24 @@ export const Resume = () => {
         google.charts.setOnLoadCallback(drawChart);
     }
 
-    const setType = (type: string) => {
-        document.getElementById("date")!.setAttribute("type", type);
+    const applyOnDate = (element: HTMLElement) => {
+        dateValue = (element as HTMLInputElement).value;
     }
 
-    if(subsequentRun) {
-        return <div id="resume">
-            <button class="btn-close"  onclick={() => setOpenState(OpenState.CLOSING)}>
-            <div class="cross"></div>
-            </button>
-            <h1>resume</h1>
-            <p class="animate-text">
-                <div id="curve_chart">{chart()}</div>
-                <input id="date"  onfocus={() => setType("month")} placeholder={data[data.length - 1][0]}/>
-                <input id="weight" type="number" step="10" value={data[data.length - 1][1]} placeholder={data[data.length - 1][1]}/>
-                <button class="btn-submit upper" onclick={() => {addData()}}>Submit</button>
-            </p>
-        </div>
-    } else {
-        return <></>
+    const applyOnWeight = (element: HTMLElement) => {
+        weightValue = parseInt((element as HTMLInputElement).value);
     }
+
+    return <div id="resume">
+        <button class="btn-close"  onclick={() => setOpenState(OpenState.CLOSING)}>
+        <div class="cross"></div>
+        </button>
+        <h1>resume</h1>
+        <p class={evaluateClass("animate-text")}>
+            <div id="curve_chart">{chart()}</div>
+            <input id="date" apply={applyOnDate} type="month" value={data[data.length - 1][0]}/>
+            <input id="weight" apply={applyOnWeight} type="number" step="10" value={data[data.length - 1][1]}/>
+            <button class="btn-submit upper" type="submit" onclick={() => {addData()}}>Submit</button>
+        </p>
+    </div>
 }
