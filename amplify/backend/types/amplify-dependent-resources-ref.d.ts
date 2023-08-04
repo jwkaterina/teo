8af44b66feaa81ebb019c212bf3f3eb17f0bd2b1
andrii.logoshko <forando@gmail.com>
2023-08-04 15:56:42 +0200

    build: Init Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "teo0d6893a8": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
