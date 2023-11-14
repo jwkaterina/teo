1710ff74b1319e2b99a08b3e4551c10da2d8def4
jwkaterina <jwkaterina@gmail.com>
2023-11-14 14:17:46 +0100

    allow fullscreen for videos

--- src/component/preview-pages/video.tsx
+++ src/component/preview-pages/video.tsx
@@ -8,7 +8,7 @@ export const Video = (): ReactishEntity => {
     return (<>
         <iframe id="player" type="text/html" width="640" height="390"
             src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${window.location.origin}`}
-            frameborder="0">
+            frameborder="0" allowfullscreen>
         </iframe>
         <button id="video-close" onclick={() => setVideoId(null)}>
             <div class="cross"></div>
