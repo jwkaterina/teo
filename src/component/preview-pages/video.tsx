518b3f21cc8d2ee4c6bc98538c58e4ada5677330
jwkaterina <jwkaterina@gmail.com>
2023-11-14 12:30:55 +0100

    get photos and videos only on open state

--- src/component/preview-pages/video.tsx
+++ src/component/preview-pages/video.tsx
@@ -7,7 +7,7 @@ export const Video = (): ReactishEntity => {
  
     return (<>
         <iframe id="player" type="text/html" width="640" height="390"
-            src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${window.location.origin}`}
+            src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${window.location.origin}`}
             frameborder="0">
         </iframe>
         <button id="video-close" onclick={() => setVideoId(null)}>
